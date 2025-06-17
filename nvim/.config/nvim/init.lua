cb7c7bab8cf1249db318b2c109ae4a49ed397f88
Jayesh Rathi <itsjayeshrathi@gmail.com>
2025-06-17 14:13:42 +0530

    made folder structure changes in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,7 +8,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 vim.opt.wrap = false
 
 -- Install Lazy.nvim if not present
@@ -24,35 +24,28 @@ vim.opt.rtp:prepend(lazypath)
 -- Load plugins
 require("lazy").setup({
   -- File Explorer
-  { "nvim-tree/nvim-tree.lua",         dependencies = { "nvim-tree/nvim-web-devicons" }},
-  -- Status line
-  { "nvim-lualine/lualine.nvim",       dependencies = { "nvim-tree/nvim-web-devicons" } },
-  -- Fuzzy Finder (Telescope)
-  { "nvim-telescope/telescope.nvim",   dependencies = { "nvim-lua/plenary.nvim" } },
-  -- Syntax highlighting
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" }},
+  -- Status Line
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }},
+  -- Fuzzy Finder
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
+  -- Syntax Highlighting & Playground
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-treesitter/playground" },
   -- LSP
   { "neovim/nvim-lspconfig" },
   -- Autocompletion
-  { "hrsh7th/nvim-cmp",                dependencies = { "hrsh7th/cmp-nvim-lsp" } },
-  -- Git integration
-  { "tpope/vim-fugitive" },
-  -- Themes
-  { "folke/tokyonight.nvim" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "rebelot/kanagawa.nvim" },
-  { "hashicorp/sentinel.vim" },
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp", -- won't be used w/o LSP but safe to include
+      "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",            -- Snippet engine
-      "saadparwaiz1/cmp_luasnip",    -- Snippet source for cmp
-      "rafamadriz/friendly-snippets" -- Optional: common snippets
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets"
     },
     config = function()
       local cmp = require("cmp")
@@ -80,52 +73,87 @@ require("lazy").setup({
       })
     end,
   },
+  -- Git Integration
+  { "tpope/vim-fugitive" },
+  -- Themes
+  { "folke/tokyonight.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "hashicorp/sentinel.vim" },
 })
 
-require("nvim-tree").setup({
-    view={
-        side = "right",
-        width = 30,
-    }
-})    
+-- Theme setup
+vim.o.background = "dark"
+vim.cmd("colorscheme kanagawa")
 
--- Theme settings
-vim.o.background = "dark"       -- Change to "light" if needed
-vim.cmd("colorscheme kanagawa") -- Default theme
-
--- Gruvbox customization (Optional)
+-- Gruvbox theme tweaks
 require("gruvbox").setup({
-  contrast = "hard", -- Options: "soft", "medium", "hard"
+  contrast = "hard",
   transparent_mode = true,
 })
 
--- Quick theme switching
-vim.keymap.set("n", "<leader>tg", ":colorscheme gruvbox<CR>", { silent = true })    -- Switch to Gruvbox
-vim.keymap.set("n", "<leader>tt", ":colorscheme tokyonight<CR>", { silent = true }) -- Switch to Tokyonight
+-- Treesitter setup
+require("nvim-treesitter.configs").setup({
+  highlight = { enable = true },
+  indent = { enable = true },
+  playground = {
+    enable = true,
+    updatetime = 25,
+    persist_queries = false,
+  },
+})
 
--- Load plugin configs
-require("nvim-tree").setup()
+-- File explorer setup
+require("nvim-tree").setup({
+  view = {
+    side = "right",
+    width = 30,
+  }
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "TSPlayground",
+  callback = function()
+    vim.cmd("wincmd L") -- Move window to the far right
+  end,
+})
+
+
+
+-- Status line
 require("lualine").setup()
 
 -- Keybindings
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })        -- File Explorer
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { silent = true }) -- Find files
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { silent = true })  -- Live Grep
-vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { silent = true })    -- Open Buffers
+
+-- File/tree/telescope
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { silent = true })
+
+-- Tree-sitter playground
+vim.keymap.set("n", "<leader>tp", ":TSPlaygroundToggle<CR>", { silent = true, desc = "Toggle TS Playground" })
+
+-- Theme switching
+vim.keymap.set("n", "<leader>tg", ":colorscheme gruvbox<CR>", { silent = true })
+vim.keymap.set("n", "<leader>tt", ":colorscheme tokyonight<CR>", { silent = true })
 
 -- Window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true }) -- Move to the left window
-vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true }) -- Move to the window below
-vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true }) -- Move to the window above
-vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true }) -- Move to the right window
+vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
 
 -- Tab navigation
-vim.keymap.set("n", "<leader>tn", ":tabnew<CR>", { silent = true })      -- Open a new tab
-vim.keymap.set("n", "<leader>to", ":tabonly<CR>", { silent = true })     -- Close all other tabs
-vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { silent = true })    -- Close current tab
-vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { silent = true }) -- Go to previous tab
-vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", { silent = true })     -- Go to next tab
+vim.keymap.set("n", "<leader>tn", ":tabnew<CR>", { silent = true })
+vim.keymap.set("n", "<leader>to", ":tabonly<CR>", { silent = true })
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { silent = true })
+vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { silent = true })
+vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", { silent = true })
 
+-- Reload current file
 vim.keymap.set("n", "<leader><leader>", ":luafile %<CR>", { silent = true })
 
+-- Notify ready
 print("Neovim is ready! 🚀")
+
